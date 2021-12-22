a8fc2cca38e00b211ddda704ab632dd407603183
samue <samue@SAMUEL>
2021-12-22 21:05:43 +0100

    Add project files.

---
+++ Sandbox1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Seahorse seahorse = new Seahorse();
+            Shell shell = new Shell();
+            IceCream icecream = new IceCream();
+            var crab = new Crab();
+        }
+    }
+}
